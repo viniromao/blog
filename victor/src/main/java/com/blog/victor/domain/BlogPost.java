cc23e56ec7c7ddf07f5ab0f0a7545d38574e7018
PraiaRural <victorluisromao@outlook.com>
2022-05-21 19:25:58 -0300

    correcao

--- victor/src/main/java/com/blog/victor/domain/BlogPost.java
+++ victor/src/main/java/com/blog/victor/domain/BlogPost.java
@@ -1,2 +1,24 @@
-package com.blog.victor.domain;public class BlogPost {
+package com.blog.victor.domain;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.Entity;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Slf4j
+public class BlogPost {
+   private String autor;
+
+   private String cabecalho;
+
+   private LocalDateTime data;
+
+   private String corpo;
+
+   private String fonte;
+
+   private String rodapé;
+
 }
